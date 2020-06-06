397a8569209964c11849436e260ffecec56f3dcc
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-06 21:21:26 +0600

    Size px Control by vue js

--- app.js
+++ app.js
@@ -29,13 +29,20 @@ var box = new Vue({
     el: '#box',
     data: {
         attachRed: false,
-        color: 'green'
+        color: 'green',
+        width: 100
     },
     computed: {
         divClasses: function () {
             return {
                 red: this.attachRed,
                 blue: !this.attachRed
+            }
+        },
+        myStyle: function(){
+            return{
+                backgroundColor: this.color,
+                width: this.width + 'px'
             }
         }
     },
