f18cd6858b26a8bee940b6382630412eb67ea22b
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-09 23:35:44 +0600

    Attack method work fine 💚

--- vueGame/js/app.js
+++ vueGame/js/app.js
@@ -7,21 +7,40 @@ new Vue({
         gameIsRunning: false
     },
     methods: {
-        startGame: function(){
+        startGame: function () {
             this.gameIsRunning = true,
             this.playerHealth = 100,
             this.monsterHealth = 100
         },
-        attack: function(){
+        attack: function () {
+            var max = 10;
+            var min = 3;
+            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.monsterHealth -= damage;
+
+            if (this.monsterHealth <= 0) {
+                alert("You Win !!!");
+                this.gameIsRunning = false;
+                return;
+            }
+
+            max = 12;
+            min = 5;
+            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            this.playerHealth -= damage;
+
+            if (this.playerHealth <= 0) {
+                alert("You Los !!!");
+                this.gameIsRunning = false;
+            }
+        },
+        specialAttack: function () {
 
         },
-        specialAttack: function(){
+        heal: function () {
 
         },
-        heal: function(){
-
-        },
-        giveUp: function(){
+        giveUp: function () {
 
         }
     },
