cd3407e6d476d515e0180c57f7036c3fe1b27fce
SOURAV ROY <souravroy.info13@gmail.com>
2020-06-09 21:50:26 +0600

    startGame Method is Done 👍✔

--- vueGame/js/app.js
+++ vueGame/js/app.js
@@ -5,5 +5,12 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false
-    }
+    },
+    methods: {
+        startGame: function(){
+            this.gameIsRunning = true,
+            this.playerHealth = 100,
+            this.monsterHealth = 100
+        }
+    },
 })
